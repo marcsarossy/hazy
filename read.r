61ed4f7d2dece38e2dfdadf147142f14164eb4c4
marc sarossy <marc@sarossy.com>
2025-11-03 20:04:39 +1100

    changed readBin to unsigned

--- read.r
+++ read.r
@@ -1,5 +1,5 @@
 z<-file('test200.img','rb') #or wherever you have this file
-y<-readBin(z,integer(),n=200*1024*200,size=1)
+y<-readBin(z,integer(),n=200*1024*200,size=1,signed=FALSE)
 dim(y)<-c(200,1024,200)  # set raw vector to correct matris
 image(t(y[100,,]))
 heatmap(y[80,,],Rowv=NA,Colv=NA,scale='none')
