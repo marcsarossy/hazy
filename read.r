6f00149fdaa800e669b2ad6846ae43d57b1b9d82
marc sarossy <marc@sarossy.com>
2025-11-03 19:57:11 +1100

    includes resizing and heatmap plot

--- read.r
+++ read.r
@@ -1,3 +1,5 @@
-z<-file('q:/ZEISS/test200.img','rb') #or wherever you have this file
+z<-file('test200.img','rb') #or wherever you have this file
 y<-readBin(z,integer(),n=200*1024*200,size=1)
+dim(y)<-c(200,1024,200)  # set raw vector to correct matris
 image(t(y[100,,]))
+heatmap(y[80,,],Rowv=NA,Colv=NA,scale='none')
